0320f5e585aa3714f6e628a5b35afabefcb714a9
Julian Curtis-Zilius <juliancurtiszilius@gmail.com>
2017-09-09 11:32:57 -0400

    Added notifications create method example

---
+++ Examples/Notifications/create.js
@@ -0,0 +1,16 @@
+// The ID of the CanvasTools Chrome Extension (Don't change this)
+var editorExtensionId = "gmdmngbeceipkojacdoljnllcnfpdgce";
+var notification = {
+    create: {
+        title: "Notification Test",
+        message: "Greeting CanvasTools from Canvas!",
+        requireInteraction: false
+    }
+}
+chrome.runtime.sendMessage(editorExtensionId, {notification: notification}, function(response) {
+    if (response.notification.create) {
+        console.log('Notification created.')
+    } else {
+    	console.log('Notification was not created, mabye the user has disabled 3rd party notifications.')
+    }
+});
