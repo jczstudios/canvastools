b2155e4a9962adf0840d13c27a0096bc44dfce7e
Julian Curtis-Zilius <juliancurtiszilius@gmail.com>
2017-11-10 08:45:47 -0500

    Update example-api-call.js

--- example-api-call.js
+++ example-api-call.js
@@ -1,8 +1,9 @@
+// ID of the CanvasTools Extension - Don't change this
 var editorExtensionId = "gmdmngbeceipkojacdoljnllcnfpdgce";
 var mySchool = {
     set: {
         title: "Test School",
-        text: "Thank you for using CanvasTools at Test High School! The school's website can be located at: https://wjccschools.org/whs/"
+        text: "Thank you for using CanvasTools at Test High School! The school's website can be located at: https://www.schoolsite.org/"
     }
 }
 chrome.runtime.sendMessage(editorExtensionId, {mySchool: mySchool}, function(response) {
